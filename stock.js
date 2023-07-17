373e8f0186609cc246eb9898b137dd1bc3d0b4e6
sobreroavilavalentina <132407168+sobreroavilavalentina@users.noreply.github.com>
2023-07-16 23:48:29 -0300

    Create stock.js

---
+++ stock.js
@@ -0,0 +1,52 @@
+const drinkType = [
+  {id: 1,type: 'Café', price: 25},
+  {id: 2,type: 'Té', price: 17},
+  {id: 3,type: 'Gaseosa', price: 22},
+  {id: 4,type: 'Agua c/s gas - Agua saborizada', price: 20},
+  {id: 5,type: 'No deseo bebida', price: 0}
+]
+
+const drinkTypeOp = [
+  {id: 1, opNum: 1,name: 'Café'},
+  {id: 1, opNum: 2,name: 'Café con leche'}, //opciones
+  {id: 1, opNum: 3,name: 'Cortado'},
+  {id: 1, opNum: 4,name: 'Capuccino'},
+  {id: 2, opNum: 1,name: 'Té'},
+  {id: 2, opNum: 2,name: 'Té de Rosas'},
+  {id: 2, opNum: 3,name: 'Té de Hierbas'},
+  {id: 2, opNum: 4,name: 'Té de Manzanilla'},
+  {id: 2, opNum: 5,name: 'Té Frutal'}, //opciones
+  {id: 3, opNum: 1,name: 'Coca Cola'},
+  {id: 3, opNum: 2,name: 'Fanta'},
+  {id: 3, opNum: 3,name: 'Sprite'},
+  {id: 4, opNum: 1,name: 'Agua sin gas'},
+  {id: 4, opNum: 2,name: 'Agua con gas'},
+  {id: 4, opNum: 3,name: 'Agua saborizada'} //opciones
+]
+
+const foodType = [
+  {id: 1, type: 'Medialuna', price: 120},
+  {id: 2, type: 'Cookie', price: 110},
+  {id: 3, type: 'Tostada', price: 145},
+  {id: 4,type: 'No deseo comida', price: 0}
+
+]
+
+const croissantOptions = [
+  {opNum: 1, name: 'Medialuna dulce'},
+  {opNum: 2, name: 'Medialuna salada'},
+  {opNum: 3, name: 'Mafalda (medialuna con queso)'}
+]
+
+const cookieOptions = [
+  {opNum: 1, name: 'Cookie con chips de chocolate'},
+  {opNum: 2, name: 'Cookie sabor limón'},
+  {opNum: 3, name: 'Cookie sabor naranja'},
+  {opNum: 4, name: 'Cookie de vainilla y ddl'},
+]
+
+const toastOptions = [
+  {opNum: 1, name: 'Tostada con palta y tomate'},
+  {opNum: 2, name: 'Tostada con queso y tomate'},
+  {opNum: 3, name: 'Tostada con queso untable y ddl/mermelada'}
+]
