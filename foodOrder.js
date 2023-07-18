a34f2d71890c6da156fda99cc1ac581cb571bae8
sobreroavilavalentina <132407168+sobreroavilavalentina@users.noreply.github.com>
2023-07-18 10:16:17 -0300

    Segunda entrega JS

--- foodOrder.js
+++ foodOrder.js
@@ -3,26 +3,6 @@ let selectedFoodType = []
 let foodOrderOption = 0
 let finalFood = 0
 
-const foodOptions = () => {
-  let foodOptionName = ''
-  let foodOptionNumber = 1
-  selectedFoodType.forEach(food => {
-    foodOptionName += `${foodOptionNumber}- ${food.name}\n`
-    foodOptionNumber++
-  })
-
-  do {
-    foodOrderOption = parseInt(prompt(`Las opciones para ${selectedFood.type} son:\n${foodOptionName}`))
-    finalFood = selectedFoodType.find(food => food.opNum === foodOrderOption)
-
-    if (!selectedFoodType.some(food => food.opNum == foodOrderOption)) {
-      alert(`debe ingresar una de las opciones disponibles en este momento. SOLO NÚMEROS`)
-    }
-  } while (!selectedFoodType.some(food => food.opNum == foodOrderOption));
- 
-  return finalFood
-}
-
 const foodOrder = () => {
   let foodId = 0;
 
@@ -34,7 +14,7 @@ const foodOrder = () => {
     4- No deseo comida
     `))
     selectedFood = foodType.find(food => food.id === foodId)
-    if (foodId > 4) {
+    if (foodId > 4 && foodId < 1) {
       alert(`debe ingresar una de las opciones disponibles en este momento. SOLO NÚMEROS`)
     }
   }
@@ -43,7 +23,7 @@ const foodOrder = () => {
 
 selectedFood = foodOrder()
 
-
+let finalTotalPrice = 0
 
 if (selectedFood.id == 1) {
   selectedFoodType = croissantOptions
@@ -52,16 +32,41 @@ if (selectedFood.id == 1) {
 } else if (selectedFood.id == 3) {
   selectedFoodType = toastOptions
 } else if (selectedFood.id == 4) {
-  alert('No desea comida')
+  finalTotalPrice = 0 + finalDrink.price
+  alert(`No desea comida.Subtotal: $${finalTotalPrice}\n${finalDrink.name} $${finalDrink.price}`)
 } else {
-  alert(`Alguno de los datos ingresados es incorrecto`);
+  alert(`Alguno de los datos ingresados es incorrecto`)
 }
 
 
 if (selectedFood.id <= 3) {
+  const foodOptions = () => {
+    let foodOptionName = ''
+    let foodOptionNumber = 1
+    selectedFoodType.forEach(food => {
+      foodOptionName += `${foodOptionNumber}- ${food.name}: $${food.price}\n`
+      foodOptionNumber++
+    })
+  
+    do {
+      foodOrderOption = parseInt(prompt(`Las opciones para ${selectedFood.type} son:\n${foodOptionName}`))
+      finalFood = selectedFoodType.find(food => food.opNum === foodOrderOption)
+  
+      if (!selectedFoodType.some(food => food.opNum == foodOrderOption)) {
+        alert(`debe ingresar una de las opciones disponibles en este momento. SOLO NÚMEROS`)
+      }
+    } while (!selectedFoodType.some(food => food.opNum == foodOrderOption))
+   
+    return finalFood
+  }
   finalFood = foodOptions()
-  alert(`Perfecto. Hemos agregado su ${finalFood.name} a su pedido.`)
+
 }
 
+finalTotalPrice = finalFood.price + finalDrink.price
+
+if (selectedFood.id <= 3) {
+  alert(`Perfecto. Hemos agregado su ${finalFood.name} a su pedido.\nSubtotal:$${finalTotalPrice}\n${finalDrink.name} $${finalDrink.price}\n${finalFood.name} $${finalFood.price}`)
+}
 
 
