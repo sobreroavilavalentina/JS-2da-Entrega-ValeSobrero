df67a65b7b14337c01be2380b8083b17a8017a2f
sobreroavilavalentina <132407168+sobreroavilavalentina@users.noreply.github.com>
2023-07-18 10:25:22 -0300

    Segunda entrega JS

--- stock.js
+++ stock.js
@@ -7,46 +7,45 @@ const drinkType = [
 ]
 
 const drinkTypeOp = [
-  {id: 1, opNum: 1,name: 'Café'},
-  {id: 1, opNum: 2,name: 'Café con leche'}, //opciones
-  {id: 1, opNum: 3,name: 'Cortado'},
-  {id: 1, opNum: 4,name: 'Capuccino'},
-  {id: 2, opNum: 1,name: 'Té'},
-  {id: 2, opNum: 2,name: 'Té de Rosas'},
-  {id: 2, opNum: 3,name: 'Té de Hierbas'},
-  {id: 2, opNum: 4,name: 'Té de Manzanilla'},
-  {id: 2, opNum: 5,name: 'Té Frutal'}, //opciones
-  {id: 3, opNum: 1,name: 'Coca Cola'},
-  {id: 3, opNum: 2,name: 'Fanta'},
-  {id: 3, opNum: 3,name: 'Sprite'},
-  {id: 4, opNum: 1,name: 'Agua sin gas'},
-  {id: 4, opNum: 2,name: 'Agua con gas'},
-  {id: 4, opNum: 3,name: 'Agua saborizada'} //opciones
+  {id: 1, opNum: 1,name: 'Café', price: 35},
+  {id: 1, opNum: 2,name: 'Café con leche', price: 40}, //opciones
+  {id: 1, opNum: 3,name: 'Cortado', price: 37},
+  {id: 1, opNum: 4,name: 'Capuccino', price: 45},
+  {id: 2, opNum: 1,name: 'Té', price: 25},
+  {id: 2, opNum: 2,name: 'Té de Rosas', price: 27},
+  {id: 2, opNum: 3,name: 'Té de Hierbas', price: 27},
+  {id: 2, opNum: 4,name: 'Té de Manzanilla', price: 28},
+  {id: 2, opNum: 5,name: 'Té Frutal', price: 28}, //opciones
+  {id: 3, opNum: 1,name: 'Coca Cola', price: 32},
+  {id: 3, opNum: 2,name: 'Fanta', price: 32},
+  {id: 3, opNum: 3,name: 'Sprite', price: 32},
+  {id: 4, opNum: 1,name: 'Agua sin gas', price: 21},
+  {id: 4, opNum: 2,name: 'Agua con gas', price: 21},
 ]
 
 const foodType = [
-  {id: 1, type: 'Medialuna', price: 120},
-  {id: 2, type: 'Cookie', price: 110},
-  {id: 3, type: 'Tostada', price: 145},
-  {id: 4,type: 'No deseo comida', price: 0}
+  {id: 1, type: 'Medialuna'},
+  {id: 2, type: 'Cookie'},
+  {id: 3, type: 'Tostada'},
+  {id: 4,type: 'No deseo comida'}
 
 ]
 
 const croissantOptions = [
-  {opNum: 1, name: 'Medialuna dulce'},
-  {opNum: 2, name: 'Medialuna salada'},
-  {opNum: 3, name: 'Mafalda (medialuna con queso)'}
+  {opNum: 1, name: 'Medialuna dulce', price: 120},
+  {opNum: 2, name: 'Medialuna salada', price: 120},
+  {opNum: 3, name: 'Mafalda (medialuna con queso)', price: 140}
 ]
 
 const cookieOptions = [
-  {opNum: 1, name: 'Cookie con chips de chocolate'},
-  {opNum: 2, name: 'Cookie sabor limón'},
-  {opNum: 3, name: 'Cookie sabor naranja'},
-  {opNum: 4, name: 'Cookie de vainilla y ddl'},
+  {opNum: 1, name: 'Cookie con chips de chocolate', price: 110},
+  {opNum: 2, name: 'Cookie sabor limón', price: 100},
+  {opNum: 3, name: 'Cookie sabor naranja', price: 100},
+  {opNum: 4, name: 'Cookie de vainilla y ddl', price: 130},
 ]
 
 const toastOptions = [
-  {opNum: 1, name: 'Tostada con palta y tomate'},
-  {opNum: 2, name: 'Tostada con queso y tomate'},
-  {opNum: 3, name: 'Tostada con queso untable y ddl/mermelada'}
+  {opNum: 1, name: 'Tostada con palta y tomate', price: 160},
+  {opNum: 2, name: 'Tostada con queso y tomate', price: 152},
+  {opNum: 3, name: 'Tostada con queso untable y ddl/mermelada', price: 145}
 ]
